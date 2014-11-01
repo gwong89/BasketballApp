33d14d6da892a8c695dcfe64063e1861a2094551
Giovanni Wong <deargiovanni@hotmail.com>
2014-11-01 14:59:15 -0700

    css files, but not working??

--- public/js/Angular/Controllers/tabs.js
+++ public/js/Angular/Controllers/tabs.js
@@ -1,12 +1,13 @@
 angular.module('basketball').controller('TabsDemoController',TabsDemoController);
 
-function TabsDemoController($scope) {
-  $scope.tabs = [
+function TabsDemoController() {
+	var vm = this;
+  vm.tabs = [
     { title:'Dynamic Title 1', content:'Dynamic content 1' },
     { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
   ];
 
-  $scope.alertMe = function() {
+ function alertMe(){
     setTimeout(function() {
       alert('You\'ve selected the alert tab!');
     });
