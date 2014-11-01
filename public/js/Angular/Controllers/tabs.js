1d9dde3daaca4a896a217b1f8208754261f4642e
Giovanni Wong <deargiovanni@hotmail.com>
2014-11-01 01:43:09 -0700

    modular code

--- public/js/Angular/Controllers/tabs.js
+++ public/js/Angular/Controllers/tabs.js
@@ -1,5 +1,6 @@
+angular.module('basketball').controller('TabsDemoController',TabsDemoController);
 
-function TabsDemoCtrl($scope) {
+function TabsDemoController($scope) {
   $scope.tabs = [
     { title:'Dynamic Title 1', content:'Dynamic content 1' },
     { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
